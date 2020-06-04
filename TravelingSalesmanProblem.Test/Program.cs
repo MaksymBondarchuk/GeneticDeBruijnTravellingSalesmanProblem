db9edf16bb90dcd2276031a2cff6c69dc093dbc1
Max Bondarchuk <bondarchuk.m.y@gmail.com>
2020-06-04 21:20:27 +0300

    Extended testing

--- TravelingSalesmanProblem.Test/Program.cs
+++ TravelingSalesmanProblem.Test/Program.cs
@@ -39,6 +39,12 @@ namespace TravelingSalesmanProblem.Test
 					results[verticesNumber - VerticesNumberFrom].Add(new List<AlgorithmResult<Chromosome>>());
 					for (int chromosomesNumber = ChromosomesNumberFrom; chromosomesNumber <= ChromosomesNumberTo; chromosomesNumber++)
 					{
+						var averageIterationsOrder = 0;
+						var averagePathOrder = 0;
+						var averageIterationsCycle = 0;
+						var averagePathCycle = 0;
+						var averageIterationsRandom = 0;
+						var averagePathRandom = 0;
 						var averageIterations = 0;
 						var averagePath = 0;
 						for (var graphCase = 0; graphCase < GraphsNumber; graphCase++)
@@ -94,20 +100,56 @@ namespace TravelingSalesmanProblem.Test
 
 							for (var test = 0; test < TestsNumber; test++)
 							{
-								AlgorithmResult<Chromosome> result = algorithm.Run(chromosomes, fitnessFunction, CrossOverFunctions.Random, mutationFunction, RunOptions.Silent);
-								results[verticesNumber - VerticesNumberFrom][degree - DegreeFrom].Add(result);
+								AlgorithmResult<Chromosome> resultOrder = algorithm.Run(chromosomes, fitnessFunction, CrossOverFunctions.Order, mutationFunction, RunOptions.Silent);
+								AlgorithmResult<Chromosome> resultCycle = algorithm.Run(chromosomes, fitnessFunction, CrossOverFunctions.Cycle, mutationFunction, RunOptions.Silent);
+								AlgorithmResult<Chromosome> resultRandom = algorithm.Run(chromosomes, fitnessFunction, CrossOverFunctions.Random, mutationFunction, RunOptions.Silent);
+								var result = new AlgorithmResult<Chromosome>
+								{
+									Result = new Chromosome
+									{
+										FitnessValue =
+											(resultOrder.Result.FitnessValue + resultCycle.Result.FitnessValue +
+											 resultRandom.Result.FitnessValue) / 3
+									},
+									LastImprovementOn = (resultOrder.LastImprovementOn + resultCycle.LastImprovementOn +
+									                     resultRandom.LastImprovementOn) / 3
+								};
+								// results[verticesNumber - VerticesNumberFrom][degree - DegreeFrom].Add(result);
 								if (test != 0 && test != TestsNumber - 1)
 								{
+									averageIterationsOrder += resultOrder.LastImprovementOn;
+									averagePathOrder += Convert.ToInt32(resultOrder.Result.FitnessValue);
+									
+									averageIterationsCycle += resultCycle.LastImprovementOn;
+									averagePathCycle += Convert.ToInt32(resultCycle.Result.FitnessValue);
+									
+									averageIterationsRandom += resultRandom.LastImprovementOn;
+									averagePathRandom += Convert.ToInt32(resultRandom.Result.FitnessValue);
+									
 									averageIterations += result.LastImprovementOn;
 									averagePath += Convert.ToInt32(result.Result.FitnessValue);
 								}
 							}
 						}
 
+						averageIterationsOrder /= TestsNumber * GraphsNumber;
+						averagePathOrder /= TestsNumber * GraphsNumber;
+
+						averageIterationsCycle /= TestsNumber * GraphsNumber;
+						averagePathCycle /= TestsNumber * GraphsNumber;
+
+						averageIterationsRandom /= TestsNumber * GraphsNumber;
+						averagePathRandom /= TestsNumber * GraphsNumber;
+
 						averageIterations /= TestsNumber * GraphsNumber;
 						averagePath /= TestsNumber * GraphsNumber;
+						
+						string pathFoundOrder = averagePathOrder <= verticesNumber * 1000 ? "Yes" : "No";
+						string pathFoundCycle = averagePathCycle <= verticesNumber * 1000 ? "Yes" : "No";
+						string pathFoundRandom = averagePathRandom <= verticesNumber * 1000 ? "Yes" : "No";
 						string pathFound = averagePath <= verticesNumber * 1000 ? "Yes" : "No";
-						Console.WriteLine($"V {verticesNumber,2}	D {degree,2}	C {chromosomesNumber, 2}	I {averageIterations, 6}	P {averagePath, 10} F? {pathFound}");
+						
+						Console.WriteLine($"V {verticesNumber,2}	D {degree,2}	C {chromosomesNumber, 2}		I {averageIterationsOrder, 6}	P {averagePathOrder, 10} F? {pathFoundOrder}	I {averageIterationsCycle, 6}	P {averagePathCycle, 10} F? {pathFoundCycle}	I {averageIterationsRandom, 6}	P {averagePathRandom, 10} F? {pathFoundRandom}	I {averageIterations, 6}	P {averagePath, 10} F? {pathFound}");
 					}
 				}
 			}
